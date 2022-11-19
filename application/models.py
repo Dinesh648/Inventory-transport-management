cb91999fe1a20d0a1bc80f0d8f7af9e897c37b89
Dinesh648 <b.dinesh29102002@gmail.com>
2022-11-19 09:39:20 +0530

    Create models.py

---
+++ application/models.py
@@ -0,0 +1,20 @@
+from .database import db
+from werkzeug.security import generate_password_hash, check_password_hash
+
+class User(db.Model):
+    class User(db.Model):
+    __tablename__ = "user"
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    username = db.Column(db.String, primary_key = True, nullable = False)
+    password_hash = db.Column(db.String(128))
+    
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+    
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
