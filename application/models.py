32628efd96cc3a4f440d6c19af1c3816ae80fb43
Dinesh648 <b.dinesh29102002@gmail.com>
2022-12-07 09:44:22 +0530

    models

--- application/models.py
+++ application/models.py
@@ -26,3 +26,35 @@ class Users(db.Model, UserMixin):
     email = db.Column(db.String(50), unique=True)
     cname = db.Column(db.String)
     password = db.Column(db.String)
+
+class Manufacturer(db.Model, UserMixin):
+    __tablename__ = "manufacturer"
+    mid = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(40),unique = True)
+    password = db.Column(db.String)
+
+class Retialer(db.Model, UserMixin):
+    __tablename__ = "retailer"
+    rid = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(40),unique = True)
+    password = db.Column(db.String)
+
+class Wholesaler(db.Model, UserMixin):
+    __tablename__ = "wholesaler"
+    wid = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(40),unique = True)
+    password = db.Column(db.String)
+
+class Products(db.Model, UserMixin):
+    __tablename__ = "products"
+    pid = db.Column(db.Integer,primary_key = True)
+    pname = db.Column(db.String(40),nullable = False,unique = True)
+    mdate = db.Column(db.String(40))
+    price = db.Column(db.Numeric,nullable = False)
+
+class Orders(db.Model, UserMixin):
+    __tablename__ = "orders"
+    userid = db.Column(db.Integer,nullable = False,primary_key = True)
+    type = db.Column(db.String(40),primary_key = True)
+    pid = db.Column(db.Integer,unique = True)
+    quantity = db.Column(db.Integer)
